54d6b4964b3bf515828524f0a82c6b55c854369b
IndominusByte <nyomanpradipta120@gmail.com>
2022-01-14 15:12:00 +0800

    add reference_id

--- db/migrations/000001_create_categories_table.up.sql
+++ db/migrations/000001_create_categories_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS categories(
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) UNIQUE NOT NULL,
-  icon VARCHAR(100) NOT NULL
+  icon VARCHAR(100) NOT NULL,
   reference_id INT
 );
